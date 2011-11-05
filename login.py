f6714e83769be115e58c0e0d72b3bba0f5b3b3d8
Saket Choudhary <saketkc@gmail.com>
2011-11-05 06:54:07 +0530

    generlaised roll number

--- login.py
+++ login.py
@@ -11,13 +11,16 @@ body = {'user': username, 'pass': password}
 headers = {'Content-type': 'application/x-www-form-urlencoded'}
 response, content = http.request(url, 'POST', headers=headers, body=urllib.urlencode(body))
 #print response['set-cookie']
+url_location = response['location']
+splitted=url_location.split("&")
+roll_no=splitted[1].split("=")[1]
 headers = {'Host': 'asc.iitb.ac.in','User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:7.0.1) Gecko/20100101 Firefox/7.0.1', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Language': 'en-us,en;q=0.5', 'Accept-Encoding': 'gzip, deflate','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7','Connection':	'keep-alive', 'Referer':'	http://asc.iitb.ac.in/academic/commjsp/displayURL.jsp?username=saket.kumar&code=09D02007&emptype=S','Cookie': response['set-cookie']}
 
 #print headers
 #url="http://asc.iitb.ac.in/academic/utility/middlecas.jsp?loginCode=09D02007&loginnumber=09D02007&loginName=Saket%20Kumar%20Choudhary&Home=ascwebsite"
 #url="http://asc.iitb.ac.in/academic/utility/middlecas.jsp?loginCode=09D02007&loginnumber=09D02007&loginName=Saket%20Kumar%20Choudhary&Home=ascwebsite"
 #url="http://libsuite.library.iitb.ac.in/iitlibdata/webopac8/opac_ldap.php?nameid=09D02007"
-url = 'http://libsuite.library.iitb.ac.in/iitlibdata/webopac8/l_renew.php?fromASC=Y&m_mem_id=09D02007&m_location_cd=1'   
+url ='http://libsuite.library.iitb.ac.in/iitlibdata/webopac8/l_renew.php?fromASC=Y&m_mem_id='+roll_no+'&m_location_cd=1'   
 response, content = http.request(url, 'POST', headers=headers)
 scraped_output = BeautifulSoup(content)
 re_for_books = re.compile("<font face=\"Arial\" size=\"2\">.*<\/font>")#\([A-Z&]\S*\s*\)+<\/font>")
@@ -42,9 +45,4 @@ print "Book", all_info["Book"][0],all_info["Book"][1],all_info["Book"][2]
 
 for info in  all_info:
     if info!="Book":
-        print info,all_info[info][0] ,all_info[info][1], all_info[info][2] 
-#for t in s:
-   #print t
-#for t in s:
- #   print t
-  #  print "#############################"
+        print info,all_info[info][0] ,all_info[info][1], all_info[info][2]
